d8c9cadfde3fd3ecb516fb5adb11b59a5ecb2921
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-26 00:44:05 +0900

    implemented and designed simple weather app

--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
 import requests
+import datetime #user need time when they search
 
 
 
 # Create your views here.
 def index(request):
+
+	if 'city' in request.POST:
+		city = request.POST['city']
+	else:
+		city = 'tokyo'
+
+	
 	URL = 'https://api.openweathermap.org/data/2.5/weather'
-	PARAMS = {'q':'tokyo','appid':appid,'units':'metric'}
+	PARAMS = {'q':city,'appid':appid,'units':'metric'}
 
 	req = requests.get(url = URL,params = PARAMS)
 	req_json = req.json()
@@ -15,5 +23,13 @@ def index(request):
 	icon = req_json['weather'][0]['icon']
 	temp = req_json['main']['temp']
 	humid = req_json['main']['humidity']
+	dt = datetime.datetime.today()
+	month = dt.strftime("%B")
+	day = dt.day
 
-	return render(request, 'weather/index.html',{'description':description,'icon':icon,'temp':temp,'humid':humid})
+	if request.method =="POST":
+		city = request.POST['city']
+		
+
+	return render(request, 'weather/index.html',{'description':description,'icon':icon,'temp':temp,
+					'humid':humid,'day':day,'city':city,'month':month})
