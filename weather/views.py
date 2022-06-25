3d1c5e1f64e22b83a1d51d7f89156ab82654fc23
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-25 21:16:16 +0900

    get json data and render it in html

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+import requests
+
+
+
 # Create your views here.
-
 def index(request):
+	URL = 'https://api.openweathermap.org/data/2.5/weather'
+	PARAMS = {'q':'tokyo','appid':appid,'units':'metric'}
 
-	return render(request, 'weather/weather.html')
+	req = requests.get(url = URL,params = PARAMS)
+	req_json = req.json()
+	description = req_json['weather'][0]['description']
+	icon = req_json['weather'][0]['icon']
+	temp = req_json['main']['temp']
+	humid = req_json['main']['humidity']
+
+	return render(request, 'weather/index.html',{'description':description,'icon':icon,'temp':temp,'humid':humid})
